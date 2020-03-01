759ee94a3d09ca8b0b169a5ffee74f9cf2d0d767
vagivagi <hagikazu7@gmail.com>
2020-03-01 18:47:00 +0900

    Add @Component annotation

--- src/main/java/org/vagivagi/blog/api/BlogProperties.java
+++ src/main/java/org/vagivagi/blog/api/BlogProperties.java
@@ -3,9 +3,11 @@ package org.vagivagi.blog.api;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "blog")
+@Component
 @Validated
 public class BlogProperties {
   @Valid

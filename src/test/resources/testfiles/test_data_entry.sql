266177a452bd99705ee1f4afa93e71c7ea8c04c5
vagivagi <hagikazu7@gmail.com>
2021-03-26 12:14:39 +0900

    Update test_data_entry.sql

--- src/test/resources/testfiles/test_data_entry.sql
+++ src/test/resources/testfiles/test_data_entry.sql
@@ -17,7 +17,7 @@ INSERT INTO entry_tag VALUES (1,'demo');
 INSERT INTO entry_tag VALUES (1,'blog');
 
 
-INSERT INTO entry VALUES (2,'Title2','Content2','vagivagi',CURRENT_TIMESTAMP(),'vagivagi',CURRENT_TIMESTAMP());
+INSERT INTO entry VALUES (2,'Title2','Content2','vagivagi','2021-04-01T21:00:00','vagivagi','2021-04-02T21:00:00');
 
 INSERT INTO category VALUES (0,2,'demo');
 

28fbec6b41a68083c8e617fbebc1dea8b73a1b78
vagivagi <hagikazu7@gmail.com>
2021-04-06 22:32:11 +0900

    Create TagRepositoryTest.java

---
+++ src/test/java/org/vagivagi/blog/api/entry/TagRepositoryTest.java
@@ -0,0 +1,18 @@
+package org.vagivagi.blog.api.entry;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+public class TagRepositoryTest {
+    @Autowired
+    private EntryRepository entryRepository;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+}

044afcffff05517893d05a8fe08bf2ffa8cdb232
vagivagi <hagikazu7@gmail.com>
2021-04-08 23:09:49 +0900

    Update TagRepositoryTest.java

--- src/test/java/org/vagivagi/blog/api/entry/TagRepositoryTest.java
+++ src/test/java/org/vagivagi/blog/api/entry/TagRepositoryTest.java
@@ -1,11 +1,19 @@
 package org.vagivagi.blog.api.entry;
 
+import am.ik.blog.entry.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -16,5 +24,12 @@ public class TagRepositoryTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-
+    @Test
+    @Sql("/testfiles/test_data_entry.sql")
+    public void success_findAll() {
+        List<Tag> tags = tagRepository.findAll();
+        assertAll(
+                () -> assertThat(tags.size()).isEqualTo(4)
+        );
+    }
 }

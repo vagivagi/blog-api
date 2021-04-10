7f9d738cf020bd61de825f589970f0a0a3e0166b
vagivagi <hagikazu7@gmail.com>
2021-04-10 22:42:00 +0900

    Update TagRepositoryTest.java

--- src/test/java/org/vagivagi/blog/api/entry/TagRepositoryTest.java
+++ src/test/java/org/vagivagi/blog/api/entry/TagRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,8 +20,6 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 public class TagRepositoryTest {
     @Autowired
     private TagRepository tagRepository;
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
     @Test
     @Sql("/testfiles/test_data_entry.sql")

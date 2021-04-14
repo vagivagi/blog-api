412d3ae0325019078b7431a63db9f11364432b81
vagivagi <hagikazu7@gmail.com>
2021-04-14 20:37:03 +0900

    Update EntryControllerTest.java

--- src/test/java/org/vagivagi/blog/api/entry/EntryControllerTest.java
+++ src/test/java/org/vagivagi/blog/api/entry/EntryControllerTest.java
@@ -23,203 +23,205 @@ import static org.mockito.BDDMockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-    properties = {
-      "spring.flyway.enabled=false",
-      "blog.github.access-token=foo",
-      "blog.github.webhook-secret=bar"
-    })
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {
+                "spring.flyway.enabled=false",
+                "blog.github.access-token=foo",
+                "blog.github.webhook-secret=bar"
+        })
 public class EntryControllerTest {
-  @LocalServerPort int port;
-  WebTestClient webClient;
-  @MockBean EntryRepository entryRepository;
+    @LocalServerPort
+    int port;
+    WebTestClient webClient;
+    @MockBean
+    EntryRepository entryRepository;
 
-  @Before
-  public void setup() {
-    this.webClient = WebTestClient.bindToServer() //
-        .baseUrl("http://localhost:" + port+ "/api") //
-        .build();
-  }
+    @Before
+    public void setup() {
+        this.webClient = WebTestClient.bindToServer() //
+                .baseUrl("http://localhost:" + port + "/api") //
+                .build();
+    }
 
-  @Test
-  public void getEntry_NotFound() throws Exception {
-    this.webClient
-        .get() //
-        .uri("/entries/999") //
-        .accept(MediaType.APPLICATION_JSON) //
-        .exchange() //
-        .expectStatus() //
-        .isNotFound() //
-        .expectBody() //
-        .jsonPath("message")
-        .isEqualTo("EntryId 999 is not found.");
-  }
+    @Test
+    public void getEntry_NotFound() throws Exception {
+        this.webClient
+                .get() //
+                .uri("/entries/999") //
+                .accept(MediaType.APPLICATION_JSON) //
+                .exchange() //
+                .expectStatus() //
+                .isNotFound() //
+                .expectBody() //
+                .jsonPath("message")
+                .isEqualTo("EntryId 999 is not found.");
+    }
 
-  @Test
-  public void getEntry() throws Exception {
-    EntryId entryId = new EntryId("100");
-    given(entryRepository.findById(entryId, false))
-        .willReturn(Optional.of(Fixtures.entry(entryId)));
+    @Test
+    public void getEntry() throws Exception {
+        EntryId entryId = new EntryId("100");
+        given(entryRepository.findById(entryId, false))
+                .willReturn(Optional.of(Fixtures.entry(entryId)));
 
-    this.webClient
-        .get() //
-        .uri("/entries/100") //
-        .accept(MediaType.APPLICATION_JSON) //
-        .exchange() //
-        .expectStatus() //
-        .isOk() //
-        .expectBody() //
-        .jsonPath("entryId")
-        .isEqualTo(100) //
-        .jsonPath("content")
-        .isEqualTo("Hello") //
-        .jsonPath("entryId")
-        .isEqualTo(100) //
-        .jsonPath("content")
-        .isEqualTo("Hello") //
-        .jsonPath("created")
-        .isMap() //
-        .jsonPath("created.name")
-        .isEqualTo("demo") //
-        .jsonPath("created.date")
-        .isNotEmpty() //
-        .jsonPath("updated")
-        .isMap() //
-        .jsonPath("updated.name")
-        .isEqualTo("demo") //
-        .jsonPath("updated.date")
-        .isNotEmpty() //
-        .jsonPath("frontMatter")
-        .isMap() //
-        .jsonPath("frontMatter.title")
-        .isEqualTo("Hello") //
-        .jsonPath("frontMatter.categories")
-        .isArray() //
-        .jsonPath("frontMatter.categories[0]")
-        .isEqualTo("foo") //
-        .jsonPath("frontMatter.categories[1]")
-        .isEqualTo("bar") //
-        .jsonPath("frontMatter.categories[2]")
-        .isEqualTo("hoge") //
-        .jsonPath("frontMatter.tags")
-        .isArray() //
-        .jsonPath("frontMatter.tags[0]")
-        .isEqualTo("a") //
-        .jsonPath("frontMatter.tags[1]")
-        .isEqualTo("b") //
-        .jsonPath("frontMatter.tags[2]")
-        .isEqualTo("c");
-  }
+        this.webClient
+                .get() //
+                .uri("/entries/100") //
+                .accept(MediaType.APPLICATION_JSON) //
+                .exchange() //
+                .expectStatus() //
+                .isOk() //
+                .expectBody() //
+                .jsonPath("entryId")
+                .isEqualTo(100) //
+                .jsonPath("content")
+                .isEqualTo("Hello") //
+                .jsonPath("entryId")
+                .isEqualTo(100) //
+                .jsonPath("content")
+                .isEqualTo("Hello") //
+                .jsonPath("created")
+                .isMap() //
+                .jsonPath("created.name")
+                .isEqualTo("demo") //
+                .jsonPath("created.date")
+                .isNotEmpty() //
+                .jsonPath("updated")
+                .isMap() //
+                .jsonPath("updated.name")
+                .isEqualTo("demo") //
+                .jsonPath("updated.date")
+                .isNotEmpty() //
+                .jsonPath("frontMatter")
+                .isMap() //
+                .jsonPath("frontMatter.title")
+                .isEqualTo("Hello") //
+                .jsonPath("frontMatter.categories")
+                .isArray() //
+                .jsonPath("frontMatter.categories[0]")
+                .isEqualTo("foo") //
+                .jsonPath("frontMatter.categories[1]")
+                .isEqualTo("bar") //
+                .jsonPath("frontMatter.categories[2]")
+                .isEqualTo("hoge") //
+                .jsonPath("frontMatter.tags")
+                .isArray() //
+                .jsonPath("frontMatter.tags[0]")
+                .isEqualTo("a") //
+                .jsonPath("frontMatter.tags[1]")
+                .isEqualTo("b") //
+                .jsonPath("frontMatter.tags[2]")
+                .isEqualTo("c");
+    }
 
-  @Test
-  public void getEntries() throws Exception {
-    given(entryRepository.findAll(any(SearchCriteria.class)))
-        .willReturn(Collections.singletonList(Fixtures.entry(new EntryId("100"))));
+    @Test
+    public void getEntries() throws Exception {
+        given(entryRepository.findAll(any(SearchCriteria.class)))
+                .willReturn(Collections.singletonList(Fixtures.entry(new EntryId("100"))));
 
-    this.webClient
-        .get() //
-        .uri("/entries") //
-        .accept(MediaType.APPLICATION_JSON) //
-        .exchange() //
-        .expectStatus() //
-        .isOk() //
-        .expectBody() //
-        .jsonPath("$")
-        .isArray() //
-        .jsonPath("$[0].entryId")
-        .isEqualTo(100) //
-        .jsonPath("$[0].content")
-        .isEqualTo("Hello") //
-        .jsonPath("$[0].created")
-        .isMap() //
-        .jsonPath("$[0].created.name")
-        .isEqualTo("demo") //
-        .jsonPath("$[0].created.date")
-        .isNotEmpty() //
-        .jsonPath("$[0].updated")
-        .isMap() //
-        .jsonPath("$[0].updated.name")
-        .isEqualTo("demo") //
-        .jsonPath("$[0].updated.date")
-        .isNotEmpty() //
-        .jsonPath("$[0].frontMatter")
-        .isMap() //
-        .jsonPath("$[0].frontMatter.title")
-        .isEqualTo("Hello") //
-        .jsonPath("$[0].frontMatter.categories")
-        .isArray() //
-        .jsonPath("$[0].frontMatter.categories[0]")
-        .isEqualTo("foo") //
-        .jsonPath("$[0].frontMatter.categories[1]")
-        .isEqualTo("bar") //
-        .jsonPath("$[0].frontMatter.categories[2]")
-        .isEqualTo("hoge") //
-        .jsonPath("$[0].frontMatter.tags")
-        .isArray() //
-        .jsonPath("$[0].frontMatter.tags[0]")
-        .isEqualTo("a") //
-        .jsonPath("$[0].frontMatter.tags[1]")
-        .isEqualTo("b") //
-        .jsonPath("$[0].frontMatter.tags[2]")
-        .isEqualTo("c");
-  }
+        this.webClient
+                .get() //
+                .uri("/entries") //
+                .accept(MediaType.APPLICATION_JSON) //
+                .exchange() //
+                .expectStatus() //
+                .isOk() //
+                .expectBody() //
+                .jsonPath("$")
+                .isArray() //
+                .jsonPath("$[0].entryId")
+                .isEqualTo(100) //
+                .jsonPath("$[0].content")
+                .isEqualTo("Hello") //
+                .jsonPath("$[0].created")
+                .isMap() //
+                .jsonPath("$[0].created.name")
+                .isEqualTo("demo") //
+                .jsonPath("$[0].created.date")
+                .isNotEmpty() //
+                .jsonPath("$[0].updated")
+                .isMap() //
+                .jsonPath("$[0].updated.name")
+                .isEqualTo("demo") //
+                .jsonPath("$[0].updated.date")
+                .isNotEmpty() //
+                .jsonPath("$[0].frontMatter")
+                .isMap() //
+                .jsonPath("$[0].frontMatter.title")
+                .isEqualTo("Hello") //
+                .jsonPath("$[0].frontMatter.categories")
+                .isArray() //
+                .jsonPath("$[0].frontMatter.categories[0]")
+                .isEqualTo("foo") //
+                .jsonPath("$[0].frontMatter.categories[1]")
+                .isEqualTo("bar") //
+                .jsonPath("$[0].frontMatter.categories[2]")
+                .isEqualTo("hoge") //
+                .jsonPath("$[0].frontMatter.tags")
+                .isArray() //
+                .jsonPath("$[0].frontMatter.tags[0]")
+                .isEqualTo("a") //
+                .jsonPath("$[0].frontMatter.tags[1]")
+                .isEqualTo("b") //
+                .jsonPath("$[0].frontMatter.tags[2]")
+                .isEqualTo("c");
+    }
 
-  @Test
-  public void searchEntries() throws Exception {
-    ArgumentCaptor<SearchCriteria> searchCriteriaArgumentCaptor =
-        ArgumentCaptor.forClass(SearchCriteria.class);
-    given(entryRepository.findAll(any(SearchCriteria.class)))
-        .willReturn(Collections.singletonList(Fixtures.entry(new EntryId("100"))));
+    @Test
+    public void searchEntries() throws Exception {
+        ArgumentCaptor<SearchCriteria> searchCriteriaArgumentCaptor =
+                ArgumentCaptor.forClass(SearchCriteria.class);
+        given(entryRepository.findAll(any(SearchCriteria.class)))
+                .willReturn(Collections.singletonList(Fixtures.entry(new EntryId("100"))));
 
-    this.webClient
-        .get() //
-        .uri(uriBuilder -> uriBuilder.path("/entries").queryParam("q", "Hello").build()) //
-        .accept(MediaType.APPLICATION_JSON) //
-        .exchange() //
-        .expectStatus() //
-        .isOk() //
-        .expectBody() //
-        .jsonPath("$")
-        .isArray() //
-        .jsonPath("$[0].entryId")
-        .isEqualTo(100) //
-        .jsonPath("$[0].content")
-        .isEqualTo("Hello") //
-        .jsonPath("$[0].created")
-        .isMap() //
-        .jsonPath("$[0].created.name")
-        .isEqualTo("demo") //
-        .jsonPath("$[0].created.date")
-        .isNotEmpty() //
-        .jsonPath("$[0].updated")
-        .isMap() //
-        .jsonPath("$[0].updated.name")
-        .isEqualTo("demo") //
-        .jsonPath("$[0].updated.date")
-        .isNotEmpty() //
-        .jsonPath("$[0].frontMatter")
-        .isMap() //
-        .jsonPath("$[0].frontMatter.title")
-        .isEqualTo("Hello") //
-        .jsonPath("$[0].frontMatter.categories")
-        .isArray() //
-        .jsonPath("$[0].frontMatter.categories[0]")
-        .isEqualTo("foo") //
-        .jsonPath("$[0].frontMatter.categories[1]")
-        .isEqualTo("bar") //
-        .jsonPath("$[0].frontMatter.categories[2]")
-        .isEqualTo("hoge") //
-        .jsonPath("$[0].frontMatter.tags")
-        .isArray() //
-        .jsonPath("$[0].frontMatter.tags[0]")
-        .isEqualTo("a") //
-        .jsonPath("$[0].frontMatter.tags[1]")
-        .isEqualTo("b") //
-        .jsonPath("$[0].frontMatter.tags[2]")
-        .isEqualTo("c");
+        this.webClient
+                .get() //
+                .uri(uriBuilder -> uriBuilder.path("/entries").queryParam("q", "Hello").build()) //
+                .accept(MediaType.APPLICATION_JSON) //
+                .exchange() //
+                .expectStatus() //
+                .isOk() //
+                .expectBody() //
+                .jsonPath("$")
+                .isArray() //
+                .jsonPath("$[0].entryId")
+                .isEqualTo(100) //
+                .jsonPath("$[0].content")
+                .isEqualTo("Hello") //
+                .jsonPath("$[0].created")
+                .isMap() //
+                .jsonPath("$[0].created.name")
+                .isEqualTo("demo") //
+                .jsonPath("$[0].created.date")
+                .isNotEmpty() //
+                .jsonPath("$[0].updated")
+                .isMap() //
+                .jsonPath("$[0].updated.name")
+                .isEqualTo("demo") //
+                .jsonPath("$[0].updated.date")
+                .isNotEmpty() //
+                .jsonPath("$[0].frontMatter")
+                .isMap() //
+                .jsonPath("$[0].frontMatter.title")
+                .isEqualTo("Hello") //
+                .jsonPath("$[0].frontMatter.categories")
+                .isArray() //
+                .jsonPath("$[0].frontMatter.categories[0]")
+                .isEqualTo("foo") //
+                .jsonPath("$[0].frontMatter.categories[1]")
+                .isEqualTo("bar") //
+                .jsonPath("$[0].frontMatter.categories[2]")
+                .isEqualTo("hoge") //
+                .jsonPath("$[0].frontMatter.tags")
+                .isArray() //
+                .jsonPath("$[0].frontMatter.tags[0]")
+                .isEqualTo("a") //
+                .jsonPath("$[0].frontMatter.tags[1]")
+                .isEqualTo("b") //
+                .jsonPath("$[0].frontMatter.tags[2]")
+                .isEqualTo("c");
 
-    verify(entryRepository, times(1)).findAll(searchCriteriaArgumentCaptor.capture());
-    SearchCriteria searchCriteria = searchCriteriaArgumentCaptor.getValue();
-    assertThat(searchCriteria.getKeyword(), is("Hello"));
-  }
+        verify(entryRepository, times(1)).findAll(searchCriteriaArgumentCaptor.capture());
+        SearchCriteria searchCriteria = searchCriteriaArgumentCaptor.getValue();
+        assertThat(searchCriteria.getKeyword(), is("Hello"));
+    }
 }
